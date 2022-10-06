b1ac2b7a52646ed7a2b8d15af2e668927dfbcb24
jalima <jacqueline.lima@capgemini.com>
2022-10-06 10:22:42 -0300

    Desafios Bootcamp CodeGirls - DIO

---
+++ src/desafiosIntermediários/CalcularDescontoLoja.java
@@ -0,0 +1,15 @@
+package desafiosIntermediários;
+
+import java.util.Scanner;
+
+public class CalcularDescontoLoja {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int desconto = input.nextInt();
+    int precoAntigo = input.nextInt();
+
+    int precoNovo = precoAntigo - (precoAntigo * desconto / 100);
+    System.out.println(precoNovo);
+  }
+}
